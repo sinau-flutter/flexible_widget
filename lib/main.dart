01ae02aa5c87e71e424e1766e9379d80a7723c37
ayogatot <azerino25@gmail.com>
2019-10-15 13:10:26 +0700

    Implement Flexible Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,52 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flexible Widget'),
         ),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.redAccent[100],
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.redAccent[200],
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                color: Colors.amber,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                color: Colors.indigo,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
